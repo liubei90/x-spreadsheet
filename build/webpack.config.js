feabd92d9ff626c5a5a826b0199bcaee0b2b95bb
刘贝 <15738376564@163.com>
2024-09-12 16:29:24 +0800

    feat: 1.1.10

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -38,13 +38,29 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 99999999,
+              name: '[name].[hash].[ext]',
+              esModule: false,
+              fallback: 'file-loader',
+            },
+          }
         ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 99999999,
+              name: '[name].[hash].[ext]',
+              esModule: false,
+              fallback: 'file-loader',
+            },
+          }
         ],
       },
     ],
